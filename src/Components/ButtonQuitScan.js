776b4f206d3ef1a055df987e21add5f6a7b0772c
Чингиз Кабыкенов <ch.kabykenov@gmail.com>
2020-07-31 21:20:47 +0600

    добавил стили для ButtonQuitScan

--- src/Components/ButtonQuitScan.js
+++ src/Components/ButtonQuitScan.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { View, Button } from "react-native";
+import { View, Button, StyleSheet } from "react-native";
 
 export const ButtonQuitScan = ({ onPress }) => {
 	return (
-		<View>
+		<View style={styles.ButtonQuitScan}>
 			<Button title="Выйти" onPress={onPress} />
 		</View>
 	);
 };
+
+const styles = StyleSheet.create({
+	ButtonQuitScan: {
+		marginBottom: 10,
+	},
+});
